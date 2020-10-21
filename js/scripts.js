1cd190416e96f67f87d7936decd43dd94dc319ba
perfectephraim <kiddgit@gmail.com>
2020-10-20 20:04:15 -0800

    created mobile responsive Slider Carousel

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,57 @@ $(document).ready(function(){
         $('.nav ul').toggleClass('showing');
 
     });
+
+
+    //for the carousel slidder 
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+//add properties for custom next and prev icons 
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 577,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+
+      });
+                      
+
+
+
 });
 
 
 /*  
-=====add project to github
+=====add project to github===============================================================
 echo "# globalprayerwalkfrontend" >> README.md
 git init
 git add README.md
